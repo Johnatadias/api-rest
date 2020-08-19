4a9df3d21ef3ded6afe7bd40662d25d7d2675f34
johnatadias <johnata.182@outloo.com>
2020-08-19 08:03:40 -0300

    Implementação swagger

--- src/main/java/br/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/br/com/produtos/apirest/resources/ProdutoResource.java
@@ -4,32 +4,51 @@ import br.com.produtos.apirest.models.Produto;
 import br.com.produtos.apirest.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API Rest Produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoResource {
 
     @Autowired
     private ProdutoRepository produtosRepository;
 
+    @ApiOperation(value = "Retorna uma lista de produtos")
     @GetMapping("/produtos")
     public List<Produto> getlListaProdutos(){
         return produtosRepository.findAll();
     }
 
+    @ApiOperation(value = "Retorna um produto único")
     @GetMapping("/produtos/{id}")
     public Produto getProdutoById(@PathVariable("id") Long id){
         return produtosRepository.findById(id).orElse(null);
     }
 
+    @ApiOperation(value = "Salvar um produto")
     @PostMapping("/produtos")
     public Produto addProduto(@RequestBody Produto produto){
         return produtosRepository.save(produto);
     }
 
+    @ApiOperation(value = "Deletar um produto")
     @DeleteMapping("/produtos/{id}")
     public ResponseEntity deletarProdutoById(@PathVariable("id") Long id){
         if(produtosRepository.existsById(id)){
@@ -39,6 +58,7 @@ public class ProdutoResource {
         return ResponseEntity.badRequest().build();
     }
 
+    @ApiOperation(value = "Atualiza um produto")
     @PutMapping("/produtos")
     public ResponseEntity deletarProdutoById(@RequestBody Produto produto){
         if(produtosRepository.existsById(produto.getId())){
